8f2b2db7da5464159099a3b25ac39f8e670a4eae
Ned Twigg <ned.twigg@diffplug.com>
2017-09-22 13:41:54 -0700

    Fix CI.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Do the Gradle build
-./gradlew build || exit 1
+./gradlew build --stacktrace || exit 1
 
-if [ "$TRAVIS_REPO_SLUG" == "diffplug/image-gradle" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "diffplug/image-grinder" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
-	./gradlew publish publishPlugins -Dgradle.publish.key=$gradle_key -Dgradle.publish.secret=$gradle_secret || exit 1
+	./gradlew publish publishPlugins -Dgradle.publish.key=$gradle_key -Dgradle.publish.secret=$gradle_secret --stacktrace || exit 1
 	# Push the javadoc
-	./gradlew gitPublishPush || exit 1
+	./gradlew gitPublishPush --stacktrace || exit 1
 fi
