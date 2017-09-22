47fbd573e66487882f728aa9da3b8c5574393737
Ned Twigg <ned.twigg@diffplug.com>
2017-09-22 03:08:41 -0700

    Fix publishing.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -3,9 +3,9 @@
 # Do the Gradle build
 ./gradlew build || exit 1
 
-if [ "$TRAVIS_REPO_SLUG" == "diffplug/goomph" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "diffplug/image-gradle" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
 	./gradlew publish publishPlugins -Dgradle.publish.key=$gradle_key -Dgradle.publish.secret=$gradle_secret || exit 1
 	# Push the javadoc
-	./gradlew publishGhPages || exit 1
+	./gradlew gitPublishPush || exit 1
 fi
